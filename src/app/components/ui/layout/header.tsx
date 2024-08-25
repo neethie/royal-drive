f69d63a577adf761fe5537e61fb1d84f547d4ebd
MoralessJ <3647693780101@ingenieria.usac.edu.gt>
2024-08-24 21:05:35 -0600

    button dark mode

--- src/app/components/ui/layout/header.tsx
+++ src/app/components/ui/layout/header.tsx
@@ -2,11 +2,15 @@
 
 import Link from "next/link";
 import { PiUserCircleThin } from "react-icons/pi";
-import { IoIosMenu } from "react-icons/io";
+import { CiDark } from "react-icons/ci";
 import { useUIStore } from "@/store";
 
 export function Header() {
     const openSideMenu = useUIStore((state) => state.openSideMenu);
+
+    const handleDarkMode = () => {
+        console.log("Dark mode");
+    };
 
     return (
         <header className="flex items-center gap-2 flex-col md:flex-row justify-between p-5 text-lg">
@@ -21,9 +25,12 @@ export function Header() {
                     <Link href="/about">Acerca</Link>
                 </nav>
             </div>
-            <div className="hidden md:flex">
-                <button>
+            <div className="flex flex-row">
+                <button className="hidden md:flex">
                     <PiUserCircleThin className="w-5 h-5 font-black" />
+                </button>
+                <button onClick={() => handleDarkMode()}>
+                    <CiDark className="w-5 h-5 font-thin" />
                 </button>
             </div>
         </header>
